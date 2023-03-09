63e00498111d47c26c756efb5fc9a69cde5a4100
Nazek-Altayeb <n.almalaika@yahoo.com>
2023-03-09 10:37:41 +0000

    update script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-
 let correctAnswer = "";
-let correctScore = incorrectScore = askedCount = 0;
-let totalQuestion = 10;
+let correctScore = incorrectScore = askedQuestions = 0;
+let totalQuestions = 10;
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     loadQuestion();
     _checkBtn.addEventListener('click', checkAnswer);
     _playAgainBtn.addEventListener('click', playAgain);
-    _totalQuestion.textContent = totalQuestion;
-    questionCounter.textContent = askedCount;
+    totalQ.textContent = totalQuestions;
+    questionCounter.textContent = askedQuestions;
 });
 
 const answers = document.querySelector('.answers');
@@ -18,11 +17,11 @@ const inCorrect = document.getElementById('incorrect');
 const _playAgainBtn = document.getElementById('play-again');
 const _result = document.getElementById('result');
 const questionCounter = document.getElementById('question-counter');
-const _totalQuestion = document.getElementById('question-sum');
+const totalQ = document.getElementById('question-sum');
 
 
 
-async function loadQuestion(){
+async function loadQuestion() {
     const APIUrl = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple';
     const result = await fetch(`${APIUrl}`)
     const data = await result.json();
@@ -31,51 +30,51 @@ async function loadQuestion(){
     showQuestion(data.results[0]);
 }
 
-function showQuestion(data){
+function showQuestion(data) {
     let question = document.getElementById('question');
     _checkBtn.disabled = false;
     correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
-    let optionsList = incorrectAnswer;
-    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+    let answersList = incorrectAnswer;
+    answersList.splice(Math.floor(Math.random() * 4), 0, correctAnswer);
 
     question.innerHTML = `${data.question} <br> `;
-   
+
     answers.innerHTML = `
-        ${optionsList.map((option, index) => `
-            <li> ${index + 1}. <span>${option}</span> </li>
+        ${answersList.map((answer, index) => `
+            <li> ${index + 1}. <span>${answer}</span> </li>
         `).join('')}
     `;
-    selectOption();
+    selectAnswer();
 }
 
-function showCategory(data){
+function showCategory(data) {
     let category = document.getElementById('category');
     category.innerHTML = `<span class = "category"> ${data.category} </span> <br>`
 }
 
-function selectOption(){
-    answers.querySelectorAll('li').forEach(function(option){
-        option.addEventListener('click', function(){
-            if(answers.querySelector('.selected')){
-                const activeOption = answers.querySelector('.selected');
-                activeOption.classList.remove('selected');
+function selectAnswer() {
+    answers.querySelectorAll('li').forEach(function (answer) {
+        answer.addEventListener('click', function () {
+            if (answers.querySelector('.selected')) {
+                let activeAnswer = answers.querySelector('.selected');
+                activeAnswer.classList.remove('selected');
             }
-            option.classList.add('selected');
+            answer.classList.add('selected');
         });
     });
 }
 
-function checkAnswer(){
+function checkAnswer() {
     _checkBtn.disabled = true;
-    if(answers.querySelector('.selected')){
+    if (answers.querySelector('.selected')) {
         let selectedAnswer = answers.querySelector('.selected span').textContent;
-        if(selectedAnswer == correctAnswer){
+        if (selectedAnswer == correctAnswer) {
             correctScore++;
         } else {
             incorrectScore++;
         }
-        checkCount();
+        countCorrectAndIncorrectAnswers();
     } else {
         _result.innerHTML = `<p>Please select an answer!</p>`;
         _checkBtn.disabled = false;
@@ -83,34 +82,34 @@ function checkAnswer(){
 }
 
 
-function checkCount(){
-    askedCount++;
-    _totalQuestion.textContent = totalQuestion;
-    questionCounter.textContent = askedCount;
+function countCorrectAndIncorrectAnswers() {
+    askedQuestions++;
+    totalQ.textContent = totalQuestions;
+    questionCounter.textContent = askedQuestions;
     correct.textContent = correctScore;
     incorrect.textContent = incorrectScore;
-    if(askedCount == totalQuestion){
-        setTimeout(function(){
+    if (askedQuestions == totalQuestions) {
+        setTimeout(function () {
             console.log("");
         }, 5000);
 
         _playAgainBtn.style.display = "block";
         _checkBtn.style.display = "none";
     } else {
-        setTimeout(function(){
+        setTimeout(function () {
             loadQuestion();
         }, 300);
     }
 }
 
 
-function playAgain(){
-    correctScore = askedCount = incorrectScore = 0;
+function playAgain() {
+    correctScore = askedQuestions = incorrectScore = 0;
     _playAgainBtn.style.display = "none";
     _checkBtn.style.display = "block";
     _checkBtn.disabled = false;
-    _totalQuestion.textContent = totalQuestion;
-    questionCounter.textContent = askedCount;
+    totalQ.textContent = totalQuestions;
+    questionCounter.textContent = askedQuestions;
     correct.textContent = correctScore;
     incorrect.textContent = incorrectScore;
     loadQuestion();
